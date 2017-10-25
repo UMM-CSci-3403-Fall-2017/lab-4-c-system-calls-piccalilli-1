497bd8efb5e7dcc1108f0df4360f9a36aa54b3de
Rocherno De Jongh <dejon076@spacewhale.morris.umn.edu>
2017-10-25 01:05:50 -0500

    Fixed all of the functions in file_disemvowel.c, added comments, more comments should probably be added. Test the script and got the right output

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -25,10 +25,16 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
      * that were copied over.
      */
 
-     int size, i;
+     int size, i, k;
+     size = 0;
+     k = 0;
      for(i = 0; i < num_chars; ++i){
+  	
+	//if the character is not a vowel, put it in the outBuffer
+	//and increment size and k
 	if(is_vowel(in_buf[i]) == false){
-		in_buf[i] = out_buf[i];
+		out_buf[k] = in_buf[i];
+		k++;
 		size++;
 	}	
      }
@@ -43,24 +49,25 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
      * in a buffer of data, copy the non-vowels to the output buffer, and
      * use fwrite to write that out. 
      */
-	FILE* outFile;
-	FILE* inFile;
-	inFile = fopen(inputFile, "r");
-	outFile = fopen(outputFile, "w+");
-        int nRead, nWrite;
+        //Defining our int variables, and in and out buffers
+	int nRead, nWrite;
 	char* inBuffer; 
 	char* outBuffer;
-
-	inBuffer = (char*) calloc(BUF_SIZE, sizeof(char));
+	
+	//Allocating memory for our buffers
+	inBuffer = (char*) calloc(BUF_SIZE + 1, sizeof(char));
 	outBuffer= (char*) calloc(BUF_SIZE, sizeof(char));
+	
+	//While that keeps putting 'chuncks' of data into a buffer, untill all data is read
+	while(0 < (nRead=fread(inBuffer, sizeof(char), BUF_SIZE, inputFile))){
 
-	while(0 < (nRead=fread(inBuffer, sizeof(char), BUF_SIZE, inFile))){
 		nWrite=copy_non_vowels(nRead, inBuffer, outBuffer); 
-		fwrite(outBuffer, 1, nWrite, outFile);
+		fwrite(outBuffer, sizeof(char), nWrite, outputFile);
 	 }
-
-	fclose(inFile);
-	fclose(outFile);
+	
+	//closing our input and output files
+	fclose(inputFile);
+	fclose(outputFile);
 
 }
 
@@ -73,6 +80,22 @@ int main(int argc, char *argv[]) {
     inputFile = stdin;
     outputFile = stdout;
 
+    //Let inputFile be the second argument
+    if(argc == 2){
+	    inputFile = fopen(argv[1], "r");
+    }
+	
+    //let inputFile be the the second argument, and outputfile the third
+    else if(argc ==  3){
+	    inputFile = fopen(argv[1], "r");
+	    outputFile = fopen(argv[2], "w");
+    }
+    else{
+	    //Return input incorrect message if there's more than 3 arguments or less than 1, and exit from the program
+	    printf("Input is incorrect");
+	    exit(0);
+    }
+	
     disemvowel(inputFile, outputFile);
 
     return 0; 
